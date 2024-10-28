6f244df99b8a3bac784cb7ab9de65ff453a6e80d
dacods <dacods.21@gmail.com>
2024-10-28 15:19:08 -0500

    Fixed lint issues

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -2,7 +2,7 @@ export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
     if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
-        throw new Error("Class extending Building must override evacuationWarningMessage")
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
 
