a3433412bb0a4b68f024f267a037e82872672e3b
dacods <dacods.21@gmail.com>
2024-10-29 11:32:41 -0500

    Created function that returns an array of students for a specific city with their new grade

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const studentGrade = newGrades.find((gradeObj) => gradeObj.studentId === student.id);
+
+      return {
+        ...student,
+        grade: studentGrade ? studentGrade.grade : 'N/A',
+      };
+    });
+}
