49296dc16343b27b1d00ac82cbb99af997b8e746
dacods <dacods.21@gmail.com>
2024-10-30 19:26:25 -0500

    Fixed

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,12 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([
+export default async function handleProfileSignup(firstName, lastName, filename) {
+  const results = await Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(filename),
-  ]).then((results) => results.map((result) => ({
+  ]);
+
+  return results.map((result) => ({
     status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason.message,
-  })));
+    value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
+  }));
 }
