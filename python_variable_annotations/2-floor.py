01287260dfd750803762e05bc5695c5bdbea75d4
dacods <dacods.21@gmail.com>
2025-01-09 14:46:41 -0600

    Added math import

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 "type-annotated function that returns the floor of the float."
+import math
 
 
 def floor(n: float) -> int:
     "Take float n and retrun floor of n"
-    return n
+    return math.floor(n)
