303562b292b2312136650c3aa73214fa3fcf1637
dacods <dacods.21@gmail.com>
2025-01-09 14:30:36 -0600

    Created new function

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"type-annotated function that returns the floor of the float."
+
+
+def floor(n: float) -> int:
+    "Take float n and retrun floor of n"
+    return floor(n)
