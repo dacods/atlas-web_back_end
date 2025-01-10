f480f993eed3cc408fe8b0dd5c58589690139106
dacods <dacods.21@gmail.com>
2025-01-10 13:20:27 -0600

    Function that take a float as argument and returns a function that multiples the float

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+takes a float as argument and returns a 
+function that multiplies a float by multiplier.
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    "Returns multiplied float"
+    def newFunction(n:float):
+        return n * multiplier
+    return newFunction
