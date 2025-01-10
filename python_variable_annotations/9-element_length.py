aa9390737b33b556042c09319911cc8b65a0b275
dacods <dacods.21@gmail.com>
2025-01-10 13:34:07 -0600

    Annotated Functions parameters and return values with appropriate types

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Annotating functoions parameters and return values
+with appropiate types
+"""
+from typing import List, Tuple, Sequence, Iterable
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    "Returns argument"
+    return lst
