ebe8f6f2736378e756474fef68db9563b37ec982
dacods <dacods.21@gmail.com>
2025-01-10 12:13:57 -0600

    Takes a list and retruns the sum as a float

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"Takes a list of floats as argument and returns the sum as a float."
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    return sum(input_list)
