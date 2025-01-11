ececa94e3ae889fd52ce3be6a546e273432c9caf
dacods <dacods.21@gmail.com>
2025-01-11 16:50:37 -0600

    Fixed Pyscodestyle

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """
-asynchronous coroutine that takes in an integer argument 
-(max_delay, with a default value of 10) named wait_random 
-that waits for a random delay between 0 and max_delay 
+asynchronous coroutine that takes in an integer argument
+(max_delay, with a default value of 10) named wait_random
+that waits for a random delay between 0 and max_delay
 (included and float value) seconds and eventually returns it.
 """
-import asyncio, random
+import asyncio
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
