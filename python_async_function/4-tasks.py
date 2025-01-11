b1f319c371aa84aff8b6fc1fab7ee4b838e08959
dacods <dacods.21@gmail.com>
2025-01-11 17:46:49 -0600

    Alterd code from 3-tasks to use task_wait_random

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,14 @@
+"""
+Altering old function to new function
+"""
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    "return the list of all the delays"
+    delays = []
+    task = [task_wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*task)
+    return sorted(delays)
