efcefcfbb140974e90ce1d158d29045945e48398
dacods <dacods.21@gmail.com>
2025-01-12 18:04:15 -0600

    Added README and created async generator

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+a coroutine that takes no arguments and will loop 10 times, each time
+asynchronously wait 1 second, then yield a random number between 0 and 10
+"""
+import asyncio
+import random
+
+
+async def async_generator() -> float:
+    """
+    coroutine will loop 10 times, each time asynchronously wait 1 second,
+    then yield a random number between 0 and 10.
+    """
+    for i in range(10):
+        num = random.uniform(0, 10)
+        await asyncio.sleep(1)
+        yield num
---
+++ python_async_comprehension/README.md
@@ -0,0 +1,1 @@
+In this project I will be continuing - Async Comprehension - in Python
