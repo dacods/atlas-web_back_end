20829118807bb4907ea2f513668766f85c39ebe4
dacods <dacods.21@gmail.com>
2025-01-12 18:45:43 -0600

    Created coroutine that executes 4 times in parallel and returns total runtime

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+coroutine that will execute four times in parallel using asyncio.gather
+"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    "Returns total runtime"
+    start = time.time()
+    parallel = await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    end = time.time()
+    total_time = end - start
+    return total_time
