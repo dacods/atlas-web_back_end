c0b7f5bd8f0a2accb2b2dd5e29b3f0d98d40742c
dacods <dacods.21@gmail.com>
2025-01-22 02:10:42 -0600

    Added documentation

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -7,14 +7,20 @@ from base_caching import BaseCaching
 
 class BasicCache(BaseCaching):
     """
-    Returns the cache data
+    Class that inherits the caching system
     """
     def put(self, key, item):
+        """
+        Assigns the item value for the key
+        """
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
+        """
+        Returns the value linked to the key
+        """
         if key is None or key not in self.cache_data:
             return
         return self.cache_data[key]
