b8e9abba9a0bec72cf92bf91e9b38960f7e6196b
dacods <dacods.21@gmail.com>
2025-01-23 05:45:01 -0600

    Created LIFO cache

---
+++ caching/2-lifo_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+LIFOCache module
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    Inherits from BaseCaching and implements LIFO caching
+    """
+    def __init__(self):
+        """
+        Initialize the class
+        """
+        super().__init__()
+        self.last = None
+
+    def put(self, key, item):
+        """
+        Add item to the cache
+        """
+        if key is None or item is None:
+            return
+        
+        if key in self.cache_data:
+            del self.cache_data[key]
+
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            del self.cache_data[self.last]
+            print(f'DISCARD: {self.last}')
+
+        self.last = key        
+
+    def get(self, key):
+        """
+        Return value linked to key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
