cb4b60f073a7c5be723245f78930befbcb94c05a
dacods <dacods.21@gmail.com>
2025-01-23 05:38:27 -0600

    Created a FIFO cache

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -15,7 +15,7 @@ class FIFOCache(BaseCaching):
         """
         super().__init__()
         self.order = []
-    
+
     def put(self, key, item):
         """
         Add item in cache using FIFO
@@ -23,18 +23,18 @@ class FIFOCache(BaseCaching):
         """
         if key is None or item is None:
             return
-        
+
         self.cache_data[key] = item
         self.order.append(key)
-        
+
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             oldest = self.order.pop(0)
             del self.cache_data[oldest]
             print(f'DISCARD: {oldest}')
-    
+
     def get(self, key):
         """
-        Return value linked to key 
+        Return value linked to key
         """
         if key is None or key not in self.cache_data:
             return None
