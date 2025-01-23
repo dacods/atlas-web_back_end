e8707ddbbbbbd15205f406a1a0282f4674e0f301
dacods <dacods.21@gmail.com>
2025-01-23 11:07:40 -0600

    Fixed pyscodestyle

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -10,7 +10,7 @@ def index_range(page: int, page_size: int) -> tuple:
     corresponding to the range of indexes to return in a list for those
     particular pagination parameters.
     """
-    
+
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
