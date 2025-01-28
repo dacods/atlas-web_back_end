cedf7bb8f224dafaf0a91755b819b61a12235729
dacods <dacods.21@gmail.com>
2025-01-28 12:34:43 -0600

    Fixed pycodestyle

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,7 +5,8 @@ Regex-ing
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                message: str, separator: str) -> str:
     """
     Function that uses regex to replace occurrences of certain fields
     """
