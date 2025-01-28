bb9ac62e6a26fb378e2deb6ddc42ba96e9c35742
dacods <dacods.21@gmail.com>
2025-01-28 13:49:52 -0600

    Fixed pycodestyle

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -9,6 +9,7 @@ from typing import List
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
+
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """
