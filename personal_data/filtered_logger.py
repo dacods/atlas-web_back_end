bbec03667237cf0a8ce7d5f787b92614f94cd8c7
dacods <dacods.21@gmail.com>
2025-01-28 12:33:18 -0600

    Annotated variables

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,9 +3,9 @@
 Regex-ing
 """
 import re
+from typing import List
 
-
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """
     Function that uses regex to replace occurrences of certain fields
     """
