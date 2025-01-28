1da694e28ba09196d1ab5b0830ed85439afdba56
dacods <dacods.21@gmail.com>
2025-01-28 13:40:00 -0600

    Added documentation to format method

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -29,6 +29,9 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """
+        Filters values in incoming log records
+        """
         record.msg = filter_datum(self.fields, self.REDACTION,
                                   record.msg, self.SEPARATOR)
         return super().format(record)
