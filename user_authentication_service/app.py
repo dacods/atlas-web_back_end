dfbd37e6672dd73bd029db21badea251a25eeb71
dacods <dacods.21@gmail.com>
2025-01-29 17:40:31 -0600

    Fixed pycodestyle

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -7,12 +7,14 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=['GET'])
 def home() -> str:
     """
     GET route for the root URL, returns a JSON response
     """
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
