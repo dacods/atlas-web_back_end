7431bec2bce0a7e240b581a2c8fcf2bd0a9f7316
dacods <dacods.21@gmail.com>
2025-01-29 17:32:00 -0600

    Moved method

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -8,7 +8,12 @@ from user import User
 from sqlalchemy.exc import NoResultFound
 
 
-
+def _hash_password(password: str) -> bytes:
+        """
+        Hashes a password using bcrypt with a generated salt.
+        """
+        salt = bcrypt.gensalt()
+        return bcrypt.hashpw(password.encode(), salt)
 
 
 class Auth:
@@ -18,13 +23,6 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def _hash_password(self, password: str) -> bytes:
-        """
-        Hashes a password using bcrypt with a generated salt.
-        """
-        salt = bcrypt.gensalt()
-        return bcrypt.hashpw(password.encode(), salt)
-
     def register_user(self, email: str, password: str) -> User:
         """
         Registers a user with a hashed password.
@@ -33,6 +31,6 @@ class Auth:
             self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            hashed_password = self.hash_password(password)
+            hashed_password = _hash_password(password)
             return self._db.add_user(email, hashed_password)
     
