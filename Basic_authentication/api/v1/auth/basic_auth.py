1669b6812f453002ec1c2080915443f8734fc2e6
dacods <dacods.21@gmail.com>
2025-01-29 11:51:09 -0600

    Added new method that returns the Base64 part of the Authorization header for a Basic Authentication

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,17 @@ class BasicAuth(Auth):
     """
     Class that inherits from Auth
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: 
+                                            str) -> str:
+        """
+        Extracts the Base64 part of the Authorization
+        header for Basic Authentication.
+        """
+        if authorization_header is None or not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        return authorization_header[len("Basic "):]
