8dbe02c3d8a1c0721c27be066ef553a5b8d6556b
dacods <dacods.21@gmail.com>
2025-01-29 11:07:43 -0600

    Fixed pycodestyle

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -11,12 +11,21 @@ class Auth():
     Class to manage the API authentication
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Determines if authentication is required for a given path.
+        """
         return False
     
 
     def authorization_header(self, request=None) -> str:
+        """
+        Retrieves the authorization header from the request.
+        """
         return None
     
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Retrieves the current authenticated user.
+        """
         return None
