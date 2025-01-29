0eed82811ae836051fa4250b940a392f78d313f3
dacods <dacods.21@gmail.com>
2025-01-29 17:18:56 -0600

    Added new method

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -3,11 +3,36 @@
 Authentication
 """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.exc import NoResultFound
 
 
-def _hash_password(password: str) -> bytes:
+
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
     """
-    Hashes a password using bcrypt with a generated salt.
-    """
-    salt = bcrypt.gensalt()
-    return bcrypt.hashpw(password.encode(), salt)
+
+    def __init__(self):
+        self._db = DB()
+
+    def _hash_password(self, password: str) -> bytes:
+        """
+        Hashes a password using bcrypt with a generated salt.
+        """
+        salt = bcrypt.gensalt()
+        return bcrypt.hashpw(password.encode(), salt)
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Registers a user with a hashed password.
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = self._hash_password(password)
+            return self._db.add_user(email, hashed_password)
+    
