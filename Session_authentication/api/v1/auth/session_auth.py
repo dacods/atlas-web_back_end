6ea235012ec5d362b00fab11c46ef9d92351fe12
dacods <dacods.21@gmail.com>
2025-01-29 15:19:39 -0600

    Updated SessionAuth

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """
 Session Authentication module
 """
+import uuid
 from api.v1.auth.auth import Auth
 
 
@@ -9,4 +10,16 @@ class SessionAuth(Auth):
     """
     Class that inherits from Auth.
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a Session ID for a user.
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
