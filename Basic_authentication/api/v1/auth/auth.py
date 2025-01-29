f58d17a1881a3113cd0ec06fb7a41960677b251e
dacods <dacods.21@gmail.com>
2025-01-29 11:05:49 -0600

    Created class to manage the API authentication

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -11,21 +11,12 @@ class Auth():
     Class to manage the API authentication
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """
-        
-        """
         return False
     
 
     def authorization_header(self, request=None) -> str:
-        """
-
-        """
         return None
     
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """
-        
-        """
         return None
