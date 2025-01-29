3cecbd697082264bfdaef06327159f9597f058b0
dacods <dacods.21@gmail.com>
2025-01-29 11:16:54 -0600

    Fixed pycodestyle

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -16,13 +16,13 @@ class Auth():
         """
         if path is None or not excluded_paths:
             return True
-        
+
         if not path.endswith('/'):
             path += '/'
-        
+
         if path in excluded_paths:
             return False
-        
+
         return True
 
     def authorization_header(self, request=None) -> str:
