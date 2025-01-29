8f44f377687cf3164c55e160da089f03e1c058d5
dacods <dacods.21@gmail.com>
2025-01-29 13:59:57 -0600

    Fixed pycodestyle

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -74,8 +74,11 @@ class BasicAuth(Auth):
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
 
-        users = User.search({"email": user_email})
-
+        try:
+            users = User.search({"email": user_email})
+        except KeyError:
+            return None
+        
         if not users or len(users) == 0:
             return None
 
