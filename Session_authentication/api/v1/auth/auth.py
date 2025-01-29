e536d729e574af80a4ceab56c26e818278c1eed8
dacods <dacods.21@gmail.com>
2025-01-29 14:54:31 -0600

    Initial commit

---
+++ Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+API authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """
+    Class to manage the API authentication
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Determines if authentication is required for a given path.
+        """
+        if path is None or not excluded_paths:
+            return True
+
+        if not path.endswith('/'):
+            path += '/'
+
+        if path in excluded_paths:
+            return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Retrieves the authorization header from the request.
+        """
+        if request is None:
+            return None
+
+        return request.headers.get("Authorization", None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Retrieves the current authenticated user.
+        """
+        return None
