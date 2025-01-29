29fa84ee607bf1dd5d70b8320e478d53218fca6e
dacods <dacods.21@gmail.com>
2025-01-29 12:17:34 -0600

    Fixed pycodestyle

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -25,14 +25,15 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[len("Basic "):]
-    
+
     def decode_base64_authorization_header(self,
                                            base64_authorization_header:
                                            str) -> str:
         """
         Decodes the Base64 part of the Authorization header.
         """
-        if base64_authorization_header is None or not isinstance(base64_authorization_header, str):
+        if base64_authorization_header is None or not isinstance(
+                                base64_authorization_header, str):
             return None
 
         try:
