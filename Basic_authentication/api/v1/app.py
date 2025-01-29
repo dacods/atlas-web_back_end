7a26237266e56ad8d907b90373c1c987b2cbed58
dacods <dacods.21@gmail.com>
2025-01-29 11:33:14 -0600

    Fixed pycodestyle

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -29,7 +29,8 @@ def before_request() -> str:
     if auth is None:
         return
 
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
 
     if not auth.require_auth(request.path, excluded_paths):
         return
