2e6fb3c243b62733a1af8b60fb53a8ae9bd23524
dacods <dacods.21@gmail.com>
2025-01-29 18:02:16 -0600

    Added new method

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -33,3 +33,13 @@ class Auth:
         except NoResultFound:
             hashed_password = _hash_password(password)
             return self._db.add_user(email, hashed_password)
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Validates a user's login credentials.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode(), user.hashed_password)
+        except NoResultFound:
+            return False
