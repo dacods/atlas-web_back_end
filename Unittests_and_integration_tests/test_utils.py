4fec5d2bc1aab03794f37c09649dde913b91bc23
dacods <dacods.21@gmail.com>
2025-02-04 20:08:55 -0600

    Added test

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -21,3 +21,15 @@ class TestAccessNestedMap(unittest.TestCase):
         Test for expected result
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """
+        Test for expected result
+        """
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(context.exception.args[0], expected)
