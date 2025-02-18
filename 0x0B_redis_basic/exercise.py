3060961d578974f53d122cb4c98a42bb5ff869ca
dacods <dacods.21@gmail.com>
2025-02-18 05:18:48 -0600

    Added 3 new methods

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,7 +4,7 @@
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -23,3 +23,26 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[bytes, str, int, float, None]:
+        """
+        
+        """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        
+        """
+        return self.get(key, lambda v: v.decode('utf-8'))
+
+    def get_int(self, key:str) -> Optional[int]:
+        """
+        
+        """
+        return self.get(key, lambda v: int(v))
