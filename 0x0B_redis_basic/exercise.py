2a50b5ba33f5975d34f15a3b59b101458426770b
dacods <dacods.21@gmail.com>
2025-02-18 06:02:17 -0600

    Added new function

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -35,6 +35,20 @@ def call_history(method: Callable) -> Callable:
         return output
     return wrapper
 
+def replay(self, method: Callable) -> None:
+    """
+    
+    """
+    input_key = f"{method.__qualname__}:inputs"
+    output_key = f"{method.__qualname__}:outputs"
+
+    inputs = self._redis.lrange(input_key, 0, -1)
+    outputs = self._redis.lrange(output_key, 0, -1)
+
+    print(f"{method.__qualname__} was called {len(inputs)} times:")
+    for input_data, output_data in zip(inputs, outputs):
+        printf(f"{method.__qualname__}(*{input_data.decode('utf-8')}) -> {output_data.decode('utf-8')}")
+
 class Cache:
     """
     
