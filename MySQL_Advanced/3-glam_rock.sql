469d11bbbdd3b657aaa32fe2d18588c0ad6d29ba
dacods <dacods.21@gmail.com>
2025-02-18 21:08:11 -0600

    Added new SQL script

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -2,7 +2,7 @@
 -- Ranked by their longevity
 SELECT
     band_name,
-    COALESCE(split, EXTRACT(YEAR FROM CURRENT_DATE)) - formed + 1 AS lifespan
+    COALESCE(split, 2024) - formed AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
