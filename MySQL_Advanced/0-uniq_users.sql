74255dc587ea5f6fcf84063f9c57ed8015a42e02
dacods <dacods.21@gmail.com>
2025-02-18 19:43:29 -0600

    Moved email and name to correct place

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Script that creates a table
 CREATE TABLE IF NOT EXISTS users(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL UNIQUE, 
-    email VARCHAR(255)
+    email VARCHAR(255) NOT NULL UNIQUE, 
+    name VARCHAR(255)
     ); 
