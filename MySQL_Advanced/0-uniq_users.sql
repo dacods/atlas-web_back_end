d2d3dbfe331cb3c075b293c82570865653600e91
dacods <dacods.21@gmail.com>
2025-02-18 19:42:26 -0600

    Moved users to correct place

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
 -- Script that creates a table
-CREATE TABLE users IF NOT EXISTS(
+CREATE TABLE IF NOT EXISTS users(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE, 
     email VARCHAR(255)
