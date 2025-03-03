aba0feed7e2c5f73e329ea50d028939a97a44423
dacods <dacods.21@gmail.com>
2025-03-03 13:05:21 -0600

    Added test using hooks

--- unittests_in_js/5-payment.test.js
+++ unittests_in_js/5-payment.test.js
@@ -1,6 +1,6 @@
 const sinon = require('sinon');
 const { expect } = require('chai')
-const sendPaymentRequestToApi = require('./5-payment')
+const sendPaymentRequestToApi = require('./3-payment')
 
 describe('sendPaymentRequestToApi', function() {
     let consoleSpy;
